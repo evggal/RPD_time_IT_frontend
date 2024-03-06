097265dae23cd9ba3ae129f92d57daeb80bc8a9f
Евгения Галеева <zenagaleeva1315@gmail.com>
2024-03-06 19:26:44 +0300

    Ииформация отныне более менее красивая

--- frontend_rpd_time_it/src/elemOnPages/infoCard/InfoCard.tsx
+++ frontend_rpd_time_it/src/elemOnPages/infoCard/InfoCard.tsx
@@ -12,14 +12,12 @@ function InfoCard () {
         <Block className="InfoCard">
             <h2 className="InfoCard__h2">
                 {data.criticalInfo.name}
-                <span>({data.criticalInfo.specialtyNumber})</span>
+                <span className="InfoCard__span"> ({data.criticalInfo.specialtyNumber})</span>
             </h2>
             <div className="InfoCard__KafInfo">
                 <p>Факультет: {data.criticalInfo.faculty}</p>
                 <p>Кафедра: {data.criticalInfo.numberOfDepartament}</p>
                 <p>Группа: {data.criticalInfo.groupName}</p>
-            </div>
-            <div  className="InfoCard__FO">
                 <p>Форма обучения: {data.criticalInfo.fo}</p>
                 <p>СПЗ: {data.criticalInfo.spz}</p>
             </div>
@@ -27,31 +25,31 @@ function InfoCard () {
             <table  className="InfoCard__Table">
                 <tr>
                     <td className="InfoCard__td">Кол. часов по курсовому проекту</td>
-                    <td className="InfoCard__td InfoCard__td_text-align-right">{data.criticalInfo.countOfHourCourseProject}</td>
+                    <td className="InfoCard__td InfoCard__td_text-align-center">{data.criticalInfo.countOfHourCourseProject}</td>
                 </tr>
                 <tr>
                     <td className="InfoCard__td">Кол. часов по курсовой работе</td>
-                    <td className="InfoCard__td InfoCard__td_text-align-right">{data.criticalInfo.countOfHourCourseWork}</td>
+                    <td className="InfoCard__td InfoCard__td_text-align-center">{data.criticalInfo.countOfHourCourseWork}</td>
                 </tr>
                 <tr>
                     <td className="InfoCard__td">Кол. часов по лекциям</td>
-                    <td className="InfoCard__td InfoCard__td_text-align-right">{data.criticalInfo.countOfHourLecture}</td>
+                    <td className="InfoCard__td InfoCard__td_text-align-center">{data.criticalInfo.countOfHourLecture}</td>
                 </tr>
                 <tr>
                     <td className="InfoCard__td">Кол. часов по практикам</td>
-                    <td className="InfoCard__td InfoCard__td_text-align-right">{data.criticalInfo.countOfHourPractice}</td>
+                    <td className="InfoCard__td InfoCard__td_text-align-center">{data.criticalInfo.countOfHourPractice}</td>
                 </tr>
                 <tr>
                     <td className="InfoCard__td">Кол. часов по лабораторным работам</td>
-                    <td className="InfoCard__td InfoCard__td_text-align-right">{data.criticalInfo.countOfHourLab}</td>
-                </tr>
-                <tr>
-                    <td className="InfoCard__td">Тип контроля</td>
-                    <td className="InfoCard__td InfoCard__td_text-align-right">{data.criticalInfo.typeOfControl}</td>
+                    <td className="InfoCard__td InfoCard__td_text-align-center">{data.criticalInfo.countOfHourLab}</td>
                 </tr>
                 <tr>
                     <td className="InfoCard__td">Самостоятельная работа студентов</td>
-                    <td className="InfoCard__td InfoCard__td_text-align-right">{data.criticalInfo.srs}</td>
+                    <td className="InfoCard__td InfoCard__td_text-align-center">{data.criticalInfo.srs}</td>
+                </tr>
+                <tr>
+                    <td className="InfoCard__td">Тип контроля</td>
+                    <td className="InfoCard__td InfoCard__td_text-align-center">{data.criticalInfo.typeOfControl}</td>
                 </tr>
             </table>
             
